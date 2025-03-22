86aaccdd0856eef1d92af2ec474d1aacc9e0b547
Dhaxx <kaiopablo44@gmail.com>
2025-03-22 01:54:10 -0300

    feat: Adiciona 'TransactionScreen' para movimentações financeiras
    - Cria tela para registrar entradas e saídas
    - Refatora 'LoginScreen' e 'HomeScreen' para melhorar organização e legibilidade

--- src/views/home.py
+++ src/views/home.py
@@ -6,19 +6,63 @@ class HomeScreen:
         self.frame = Frame(self.root)
         self.root.title('ERPy | Home')
 
-        self.frame = Frame(width=350, height=350, bg='white')
+        self.frame = Frame(width=350, height=350)
         self.frame.pack(fill='both', expand=True)
         # Configuração de responsividade
         self.frame.grid_rowconfigure(0, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
 
-        self.root.bind('<Configure>', self.on_resize)
+        # Posiciona Side bar e seus componentes
+        self.side_bar = SideMenu(self.frame, home_screen=self)
 
-    def on_resize(self, e):
-        width = self.root.winfo_width()
-        height = self.root.winfo_height()
-
-        self.heading.grid(padx=width//2 - 100, pady=height//2 - 50)
+        self.bottom_frame = Frame(self.frame, bg='lightgray', height=20, width=300)
+        self.bottom_frame.pack(side='bottom', fill='x')
+        empresa_label = Label(self.bottom_frame, text='EMPRESA - ', fg=primary_color, font=('Microsoft YaHei UI Light', 10, 'bold', 'italic'), bg='lightgray')
+        nome_empresa_label = Label(self.bottom_frame, text='KBTecnologia ME', font=('Microsoft YaHei UI Light', 11, 'italic'), bg='lightgray')
+        nome_empresa_label.pack(side='right', padx=(0,20))
+        empresa_label.pack(side='right')
+        
+        # Posiciona Frame de conteúdo
+        self.content = Frame(self.frame)
+        self.content.pack(fill='both', expand=True)
 
     def show(self):
         self.root.deiconify()
+
+    def showTransactionScreen(self):
+        for widget in self.content.winfo_children():
+            widget.destroy()
+        self.root.title('ERPy | Movimentação')
+        transaction_frame = TransactionScreen(self.content)
+        self.content = transaction_frame
+
+class SideMenu(Frame):
+    def __init__(self, master, home_screen):
+        super().__init__(master, width=250, bg='white')
+        self.home_screen = home_screen
+        self.pack(side='left', fill='y')
+
+        Frame(self, height=20, bg='white').pack(side='top')
+        Label(self, text='ERPy', padx=50, fg=primary_color, bg="white",
+              font=('Microsoft YaHei UI Light', 23, 'bold')).pack(side='top')
+        Label(self, text='Gestão Inteligente para o Seu Negócio', fg='black',
+              bg='white', font=('Microsoft YaHei UI Light', 6)).pack(side='top')
+        Frame(self, height=20, bg='white').pack(side='top')
+
+        SideMenuButtons(self, text='Movimentação', action=home_screen.showTransactionScreen)
+        SideMenuButtons(self, text='Balanço')
+
+class SideMenuButtons(Button):
+    def __init__(self, master, **kwargs):
+        command = kwargs['action'] if 'action' in kwargs else None
+        super().__init__(master, text=kwargs['text'], padx=8, pady=5, border=0, width=20, bg=primary_color, fg='white', cursor='hand2',command=command)
+        self.pack(side='top', pady=10)
+
+class TransactionScreen(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.pack(fill='both', expand=True)
+
+        self.insert_btn = Button(self, text='Inserir')
+        self.insert_btn.pack(side='top')
--- src/views/login.py
+++ src/views/login.py
@@ -15,15 +15,35 @@ class LoginScreen:
         self.background_img.place(x=0, y=0)
         self.img = img  # Manter uma referência para evitar que a imagem seja coletada pelo garbage collector
 
-        self.frame = Frame(width=350, height=350, bg='white')
+        self.frame = LoginForm(self.root, login_screen=self)
         self.frame.place(x=540, y=70)
+    
+    def show_error(self, message):
+        messagebox.showerror('Erro', message)
 
-        heading = Label(self.frame, text='ERPy', fg=primary_color, bg="white", font=('Microsoft YaHei UI Light', 23, 'bold'))
+    def hide(self):
+        self.background_img.destroy()
+        self.frame.destroy()
+        self.img.blank()
+        self.root.withdraw()
+
+    def on_login(self):
+        username = self.frame.username.get()
+        password = self.frame.password.get()
+
+        self.controller.login(username, password)
+
+class LoginForm(Frame):
+    def __init__(self, master, login_screen):
+        super().__init__(master, width=350, height=350, bg='white')
+
+        heading = Label(self, text='ERPy', fg=primary_color, bg="white", font=('Microsoft YaHei UI Light', 23, 'bold'))
         heading.place(x=130, y=30)
-        subheading = Label(self.frame, text='Gestão Inteligente para o Seu Negócio', fg='black', bg='white', font=('Microsoft YaHei UI Light', 9))
+        subheading = Label(self, text='Gestão Inteligente para o Seu Negócio', fg='black', bg='white', font=('Microsoft YaHei UI Light', 9))
         subheading.place(x=65, y=75)
 
-        self.username = Entry(self.frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11))
+        # Implementa campo de entrada de usuário
+        self.username = Entry(self, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11))
         self.username.place(x=70, y=140)
         self.username.insert(0, 'User')
         self.username.bind('<FocusIn>', self.on_enter)
@@ -34,10 +54,11 @@ class LoginScreen:
         username_icon = PhotoImage(file=username_icon_path)
         self.username_icon = username_icon
 
-        Label(self.frame, image=username_icon, bg="white", width=23, height=23).place(x=42, y=138)
-        Frame(self.frame, width=204, height=2, bg='black').place(x=69, y=163)
+        Label(self, image=username_icon, bg="white", width=23, height=23).place(x=42, y=138)
+        Frame(self, width=204, height=2, bg='black').place(x=69, y=163)
 
-        self.password = Entry(self.frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11), show='*')
+        # Implementa campo de entrada para senha
+        self.password = Entry(self, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11), show='*')
         self.password.place(x=70, y=200)
         self.password.insert(0, 'Password')
         self.password.bind('<FocusIn>', self.on_enter_password)
@@ -47,24 +68,24 @@ class LoginScreen:
         password_icon = PhotoImage(file=password_icon_path)
         self.password_icon = password_icon
 
-        Label(self.frame, image=password_icon, bg="white", width=23, height=23).place(x=42, y=197)
-        Frame(self.frame, width=204, height=2, bg='black').place(x=69, y=223)
+        Label(self, image=password_icon, bg="white", width=23, height=23).place(x=42, y=197)
+        Frame(self, width=204, height=2, bg='black').place(x=69, y=223)
 
+        # Botão de visualização de senha
         self.visibility_icon_path_visible = find_picture('../static/icons/visible.png')
         self.visibility_icon_path_invisible = find_picture('../static/icons/invisible.png')
         self.visibility_icon = PhotoImage(file=self.visibility_icon_path_visible)
         self.visibility_password_btn = Button(
-            self.frame, width=28, image=self.visibility_icon, bg='white', border=0, cursor='hand2', command=self.toggle_password_visibility
+            self, width=28, image=self.visibility_icon, bg='white', border=0, cursor='hand2', command=self.toggle_password_visibility
         )
         self.visibility_password_btn.place(x=260, y=200)
-
-        Button(self.frame, width=39, pady=7, text='Login', bg=primary_color, fg='white', border=0, cursor='hand2', command=self.on_login).place(x=35, y=274)
-        label = Label(self.frame, text='Não possui acesso?', fg='black', bg='white', font=('Microsoft YaHei UI Light', 7))
-        label.place(x=90, y=310)
-
         self.password_visible = False
 
-        btn_recrutador = Button(self.frame, width=20, text='Entrar como recrutador', border=0, fg=primary_color, bg='white', font=(('Microsoft YaHei UI Light', 7)), cursor='hand2', command=self.guest_login)
+        Button(self, width=39, pady=7, text='Login', bg=primary_color, fg='white', border=0, cursor='hand2', command=login_screen.on_login).place(x=35, y=274)
+        label = Label(self, text='Não possui acesso?', fg='black', bg='white', font=('Microsoft YaHei UI Light', 7))
+        label.place(x=90, y=310)
+
+        btn_recrutador = Button(self, width=20, text='Entrar como recrutador', border=0, fg=primary_color, bg='white', font=(('Microsoft YaHei UI Light', 7)), cursor='hand2', command=self.guest_login)
         btn_recrutador.place(x=175, y=310)
 
     def on_enter(self, e):
@@ -106,21 +127,6 @@ class LoginScreen:
         if current_text != validated_text:
             self.username.delete(0, 'end')
             self.username.insert(0, validated_text)
-
-    def on_login(self):
-        username = self.username.get()
-        password = self.password.get()
-
-        self.controller.login(username, password)
-
-    def show_error(self, message):
-        messagebox.showerror('Erro', message)
-
-    def hide(self):
-        self.background_img.destroy()
-        self.frame.destroy()
-        self.img.blank()
-        self.root.withdraw()
 
     def guest_login(self):
         self.username.delete(0, 'end')
